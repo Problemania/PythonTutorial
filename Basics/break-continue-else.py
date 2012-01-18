158684feb4828c6c4ac0998e25e907f2044dde48
Problemaniac@gmail.com <Problemaniac@gmail.com>
2012-01-17 20:54:53 -0600

    first commit

---
+++ Basics/break-continue-else.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+################################################################################ 
+## Summary: simple Python program demonstrating break and continue statement,
+## and else clause on loops.
+##
+################################################################################ 
+
+
+
+
+
+
+
+## END
+
---
+++ README.markdown
@@ -0,0 +1,1 @@
+Some simple Python programs.
