40acc85cfaed604df0c8a2ae2244f22298b4eebd
Problemaniac@gmail.com <Problemaniac@gmail.com>
2012-01-19 02:49:14 -0600

    Improved re-space.py

--- Basics/re-space.py
+++ Basics/re-space.py
@@ -1,35 +1,57 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-## Summary: Find Unicode characters matching regexp '\r' (in re) and
+## * Summary: Find Unicode characters matching regexp '\r' (in re) and
 ## '[[:space:]]' in regex. Usage of char() and unichr().
 ## 
+## * Python doc:
+##   - http://docs.python.org/library/functions.html#unichr
+##   - http://docs.python.org/library/functions.html#ord
+##   - http://docs.python.org/library/functions.html#xrange
+##
+## * Non-break space
+##   - http://en.wikipedia.org/wiki/Non-breaking_space
+##
 ##
 
 ##
-## also we go up to 0x10ffff (inclusive) because that's what help(unichr) says.
-##
-## * Python doc:
-##     http://docs.python.org/library/functions.html#unichr
-##
 ## * Remarks:
 ##   - c is Unicode code
+##   - xrange() is advantageous than range() since most elements aren't used
 ## 
+
 #unicodeChars = [unichr(c) for c in range(0x10FFFF+1)] # wide Python build
-unicodeChars = [unichr(c) for c in range(0xFFFF+1)] # narrow Python build
-str = ''.join(unicodeChars)
+#unicodeChars = [unichr(c) for c in range(0xFFFF+1)] # narrow Python build
+import sys
+str = u''.join(unichr(c) for c in xrange(sys.maxunicode+1)) # generic, with sys
 
 ##
 ## Find characters matching `\s` with `re` module:
 ## * Output:
 ##     [u'\t', u'\n', u'\x0b', u'\x0c', u'\r', u' ']
 ##
+print("Use re:")
 import re
-whiteSpaceChar=re.findall('\s',str)
-print(whiteSpaceChar)
+whiteSpaceChar=re.findall(r'\s',str)
+print "Whitespace characters matching \s using re: ", whiteSpaceChar
 
-whiteSpaceCode=[ord(char) for char in whiteSpaceChar]
-print(whiteSpaceCode)
+## Include Non-break space
+allWhiteSpaceChar=re.findall(r'\s', str, re.UNICODE)
+print "Whitespace characters matching \s using re.UNICODE: ", allWhiteSpaceChar
+
+
+from unicodedata import name
+print "Unicode ordinal, ", "Unicode string literal, ", "Unicode character name"
+for char in allWhiteSpaceChar:
+  print ord(char), repr(char), name(char, '')
+
+
+
+
+
+
+
+
 
 ##
 ## Find characters matching `[[:space:]]` with `regex` module:
@@ -38,6 +60,7 @@ print(whiteSpaceCode)
 ##   - Macports:
 ##       $ sudo port install py27-regex
 ##
+print("\n\nUse regex:")
 import regex
 spaceChar=regex.findall("[[:space:]]",str)
 print(spaceChar)
