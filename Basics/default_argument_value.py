a0a9939682e730dcd32918bfa56904f6662f195d
Problemaniac <problemaniac@gmail.com>
2012-01-31 23:33:59 -0500

    +Added example to set function argument default value to None.

--- Basics/default_argument_value.py
+++ Basics/default_argument_value.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3.2
 # -*- coding: utf-8 -*-
 
-## the list object is assigned to a default value
-## and it only evaluates once
+## Summary: simple example program to show default value assignment for
+## function argument
+
+## -----------------------------------------------------------------------------
+## The list object is assigned to a default value
+## and it only evaluates once.  The list object is mutated in the function.
 def f1(a, l=[]):
     l.append(a)
     return(l)
 
-## l is set to default value [] then 1 is appended to it
+## l is set to default value [] then 1 is appended to it by mutating the list.
 print(f1(1)) # output: [1]
+
 ## l is [1] and the second 1 is appended to it
 print(f1(1)) # output: [1, 1]
+
 ## l is [1, 1] and the third is appended to it
 print(f1(1)) # output: [1, 1, 1]
 
-# but using rebinding will not give 
+
+## -----------------------------------------------------------------------------
+## But using rebinding will not give 
 def f2(a, b=1):
     b = b + 1 # rebinding, *not* mutation
     return(a+b)
@@ -30,5 +38,25 @@ print(f2(1)) # output: 3
 
 ## The same thing happened again.
 print(f2(1)) # output: 3
+
+
+## -----------------------------------------------------------------------------
+## Use None and condtional to avoid sharing the same object between different
+## function calls.  l is set to None each time the function is called.
+def f3(a, l=None):
+    if l == None:
+        print("l == None, so this is run!")
+        l = []
+    l.append(a)
+    return(l)
+
+## l is set to default value [] then 1 is appended to it by mutating the list.
+print(f3(1)) # output: [1]
+
+## l is [1] and the second 1 is appended to it
+print(f3(1)) # output: [1]
+
+## l is [1, 1] and the third is appended to it
+print(f3(1)) # output: [1]
 
 ## END
